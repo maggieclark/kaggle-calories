557203a6e262afd0886b7144bd88ed3488ddab51
maggieclark <39098852+maggieclark@users.noreply.github.com>
2025-05-02 13:25:39 -0400

    Create lin-reg.R

---
+++ lin-reg.R
@@ -0,0 +1,29 @@
+library(readr)
+library(readxl)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+train = read_csv('train.csv')
+
+# create folds
+
+set.seed(117)
+
+fold1 <- train %>% slice_sample(n=150000)
+remaining1  <- anti_join(train, fold1, by = 'id')
+
+fold2 <- remaining1 %>% slice_sample(n=150000)
+remaining2  <- anti_join(remaining1, fold2, by = 'id')
+
+fold3 <- remaining2 %>% slice_sample(n=150000)
+remaining3  <- anti_join(remaining2, fold3, by = 'id')
+
+fold4 <- remaining3 %>% slice_sample(n=150000)
+remaining4  <- anti_join(remaining3, fold4, by = 'id')
+
+fold5 <- remaining4
+
+rm(remaining, remaining1, remaining2, remaining3, remaining4)
